cc1b80fc3f192846a145ed32c19d779cda7cb305
Jaivin Wylde <jaivinwylde@icloud.com>
2021-10-11 11:54:38 -0700

    setup website

---
+++ website.tf
@@ -0,0 +1,44 @@
+locals {
+  bucket = "jai.vin"
+  region = "sfo3"
+}
+
+/*
+  Setup the website foundation.
+*/
+
+// Create the bucket
+resource "digitalocean_spaces_bucket" "site_bucket" {
+  name   = local.bucket
+  region = local.region
+}
+
+// Add the files
+resource "digitalocean_spaces_bucket_object" "site" {
+  bucket       = local.bucket
+  region       = local.region
+  acl          = "public-read"
+  key          = "index.html"
+  content      = "<html><body>jai.vin</body></html>"
+  content_type = "text/html"
+}
+
+/*
+  Setup the website DNS records.
+*/
+
+// Create the record for the root
+resource "cloudflare_record" "site_dns" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.domain
+  type    = "CNAME"
+  value   = "${local.bucket}.${local.region}.digitaloceanspaces.com"
+}
+
+// Create a redirect record for www
+resource "cloudflare_record" "www_dns" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "www"
+  type    = "CNAME"
+  value   = "@"
+}
