9b0099907e422649ebf10e13ebbb749803f0406b
Jaivin Wylde <jaivinwylde@icloud.com>
2021-10-11 12:00:33 -0700

    changed root domain to @

--- website.tf
+++ website.tf
@@ -30,7 +30,7 @@ resource "digitalocean_spaces_bucket_object" "site" {
 // Create the record for the root
 resource "cloudflare_record" "site_dns" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
-  name    = var.domain
+  name    = "@"
   type    = "CNAME"
   value   = "${local.bucket}.${local.region}.digitaloceanspaces.com"
 }
