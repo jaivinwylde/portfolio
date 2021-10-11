b574cc49289bbd4209e7db8194f0426b33699fc0
Jaivin Wylde <jaivinwylde@icloud.com>
2021-10-11 11:30:13 -0700

    add versions

--- main.tf
+++ main.tf
@@ -2,21 +2,28 @@
 terraform {
   backend "s3" {
     bucket = "waifudex-terraform"
-    key    = "state/terraform.tfstate"
+    key    = "jai.vin/terraform.tfstate"
     region = "us-east-1"
   }
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
 }
 
 // Initialize the providers
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
+  version = "~> 3.0"
 }
 provider "cloudflare" {}
+provider "digitalocean" {}
 
 // Get the cloudflare zone
 data "cloudflare_zones" "domain" {
