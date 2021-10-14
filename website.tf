e48914783d659361a6c0986be59695bf598732c6
Jaivin Wylde <jaivinwylde@icloud.com>
2021-10-13 17:12:20 -0700

    terraform folder wasnt working

--- website.tf
+++ website.tf
@@ -59,6 +59,7 @@ resource "aws_s3_bucket_object" "site" {
   key          = each.key
   content_type = each.value.content_type
   source       = each.value.source_path
+  content      = each.value.content
 
   etag = each.value.digests.md5
 }
