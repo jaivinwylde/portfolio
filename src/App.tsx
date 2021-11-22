f385502a12f5c69859f14beca0c92e0300d4c419
Jaivin Wylde <jaivinwylde@icloud.com>
2021-11-22 15:15:47 -0800

    adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { WorkRng } from "pages/workrng"
+
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 export function App() {
-  return <>sup</>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>sup</>} />
+        <Route path="/workrng" element={<WorkRng />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
