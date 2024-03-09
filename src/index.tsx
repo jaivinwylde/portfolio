3e2948bc700f44d815e12e641664b063de3ed5b3
Jaivin Wylde <me@jai.vin>
2024-03-08 22:05:35 -0800

    if you're reading this, have a great day

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import "./App.css"
 import React from "react"
 import { render } from "react-dom"
 
-// const body = document.querySelector("body") as any
-// body.style.backgroundColor = "#121212"
-
 render(
   <React.StrictMode>
     <App />
